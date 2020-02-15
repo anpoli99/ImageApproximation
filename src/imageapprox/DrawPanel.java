6ad08fe3e8a71c92f028781bdcf39b7f2954c479
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2020-02-15 16:03:56 -0500

    Update DrawPanel.java

--- src/imageapprox/DrawPanel.java
+++ src/imageapprox/DrawPanel.java
@@ -97,37 +97,31 @@ public class DrawPanel extends JPanel implements MouseMotionListener, ActionList
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	} 
 }
